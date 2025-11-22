5b9ff8bf31a77352b24dc4c1a30a8144ea211774
mitchell <mitchell.rian.smith@gmail.com>
2025-11-22 09:48:55 -0800

    changed base url env.variable

--- src/app/test_ssr/page.js
+++ src/app/test_ssr/page.js
@@ -10,7 +10,7 @@ import React from 'react';
  * 
  */
 async function getHello() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hello`);
+  const res = await fetch(`${process.env.BASE_URL}/api/hello`);
   const data = await res.json();
   return data;
 }
