f745508642d9f1d1877f214ed542a37f2e375149
mitchell <mitchell.rian.smith@gmail.com>
2025-11-23 16:21:01 -0800

    proxy file now stores public pages in a variable, not private. Makes it easier to make the whole site private

--- src/proxy.js
+++ src/proxy.js
@@ -1,9 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/test_ssr', '/test_csr'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
 })
 
 
