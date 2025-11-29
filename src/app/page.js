efde8ab9605883d39ccd349e824a202c30e2f4c7
mitchell <mitchell.rian.smith@gmail.com>
2025-11-29 06:35:09 -0800

    Update home page header for checking public/private page

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.intro}>
-          <h1>Hello World</h1>
+          <h1>home</h1>
         </div>
       </main>
     </div>
