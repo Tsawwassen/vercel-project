c5f29f2541dfed6e210bfe32514d47640e0f892a
mitchell <mitchell.rian.smith@gmail.com>
2025-11-29 06:23:12 -0800

    had the not signed in li tags in the wrong spot

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,12 +34,12 @@ export default function RootLayout({ children }) {
             <ul>
             <SignedOut>
               <SignInButton />
-              <SignUpButton>
-                <li><button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              <li><SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
-                </button></li>
-                <li><a href='#'>Not Signed In</a></li>
-              </SignUpButton>
+                </button>
+              </SignUpButton></li>
+              <li><a href='#'>Not Signed In</a></li>
             </SignedOut>
             <SignedIn>
               <UserButton />
