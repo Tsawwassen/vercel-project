bd73f219c0208d5e13651a6723cfca55cc19f0c7
mitchell <mitchell.rian.smith@gmail.com>
2025-11-29 08:56:38 -0800

    Add dotenv to file to acccess env variable

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,8 @@
+// src/lib/db.ts
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
 
 const sql = neon(process.env.DATABASE_URL!);
-
 export const db = drizzle(sql);
